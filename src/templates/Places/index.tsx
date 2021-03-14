c96012506339f21c9b9f5af94adad048cd0ee3f9
Nathanael Cruz <nathanael.cruz.fd@gmail.com>
2021-03-14 13:36:05 -0300

    Testing configuration

---
+++ src/templates/Places/index.tsx
@@ -0,0 +1,32 @@
+type ImageProps = {
+  url: string
+  height: number
+  width: number
+}
+
+export type PlaceTemplateProps = {
+  place: {
+    slug: string
+    name: string
+    description: {
+      html: string
+    }
+    gallery: ImageProps[]
+  }
+}
+
+export default function PlacesTemplate({ place }: PlaceTemplateProps) {
+  return (
+    <>
+      <h1>{place.name}</h1>
+      {place.description !== null ? (
+        <div dangerouslySetInnerHTML={{ __html: place.description.html }} />
+      ) : (
+        'hhe'
+      )}
+      {place.gallery.map((image, index) => (
+        <img key={`photo-${index}`} src={image.url} alt={place.name} />
+      ))}
+    </>
+  )
+}
